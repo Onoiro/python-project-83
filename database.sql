a96a1d04f38c030abf7053049a9a58c604bfe489
Andrey Bogatyrev <t2way@yandex.ru>
2023-07-10 17:37:35 +0300

    modify database.sql: add CREATE TABLE url_checks

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE urls (
     name VARCHAR(255) UNIQUE,
     created_at timestamp NOT NULL
 );
+
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls (id),
+    status_code integer,
+    h1 text,
+    title text,
+    description text,
+    created_at timestamp NOT NULL
+);
