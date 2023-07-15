de184885cc78167ab2d5e3dbcedc40697db5442d
Andrey Bogatyrev <t2way@yandex.ru>
2023-07-15 15:40:10 +0300

    update app.py: correct checks(id): add UPDATE urls with last_check=check_created_at

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -106,10 +106,12 @@ def checks(id):
     check_created_at = date.today()
     flash('Страница успешно проверена', 'success')
     cur.execute('INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s) \
-                     RETURNING id', (url_id, check_created_at))   
+                     RETURNING id', (url_id, check_created_at))
     conn.commit()
     check_url_data = cur.fetchone()
     check_id = check_url_data['id']
+    cur.execute('UPDATE urls SET last_check = %s WHERE id = "id"', (check_created_at, ))
+    conn.commit()
     return redirect(url_for('url',
                             check_id=check_id,
                             url_id=url_id,
