74f8725267e656bf2883b80e7ba6cfc1db35e69e
Andrey Bogatyrev <t2way@yandex.ru>
2023-08-19 22:03:15 +0300

    update urls.py: make if/else url_data instead try/except in get_correct_url()

--- page_analyzer/urls.py
+++ page_analyzer/urls.py
@@ -22,16 +22,18 @@ def get_correct_url(url):
     url_parts = urlparse(url)
     url = f"{url_parts.scheme}://{url_parts.netloc}"
     url_data = get_url_by_name(url)
-    # if url not exist url_data will get None so need except TypeError
-    try:
+    if url_data:
         url_id = url_data['id']
         message = 'Страница уже существует'
         category = 'info'
-    # if URL not exist
-    except TypeError:
+    else:
         message = 'Страница успешно добавлена'
         category = 'success'
         created_at = date.today()
         url_data = add_url(url, created_at)
         url_id = url_data['id']
     return url_id, message, category
+
+
+def get_correct_check(id):
+    pass
