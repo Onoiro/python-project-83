705bd9765c19723fbb84ae0037985f9c6dabd101
Andrey Bogatyrev <t2way@yandex.ru>
2023-08-29 22:56:07 +0300

    correct urls.py: create get_url_data(url): remove getting url & url_data from get_correct_url(url)

--- page_analyzer/urls.py
+++ page_analyzer/urls.py
@@ -16,9 +16,7 @@ def get_correct_url(url):
     if not validators.url(url):
         message = 'Некорректный URL'
         return status, message, category
-    url_parts = urlparse(url)
-    url = f"{url_parts.scheme}://{url_parts.netloc}"
-    url_data = get_url_by_name(url)
+    url_data = get_url_data(url)
     if url_data:
         url_id = url_data['id']
         message = 'Страница уже существует'
@@ -30,3 +28,10 @@ def get_correct_url(url):
         url_data = add_url(url, created_at)
         url_id = url_data['id']
     return url_id, message, category
+
+
+def get_url_data(url):
+    url_parts = urlparse(url)
+    url = f"{url_parts.scheme}://{url_parts.netloc}"
+    url_data = get_url_by_name(url)
+    return url_data
